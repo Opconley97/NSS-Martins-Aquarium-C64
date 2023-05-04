f54eb0a045b9c3b45eb7ce4ae47cc2c5d0219629
Oliver Conley <opconley97@gmail.com>
2023-05-04 12:02:27 -0700

    Completed database, except for imgs.

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,91 @@
 const database = {
     fishes = [
         {
-            id: ,
-            species: "",
-            length: ,
-            name: "",
-            locationId: ,
-            diet: ""
+            id: 1,
+            species: "Fish 1",
+            length: 1,
+            name: "Fish 1",
+            locationId: 1,
+            diet: "Food",
+            img: ""
         },
+        {
+            id: 2,
+            species: "Fish 2",
+            length: 2,
+            name: "Fish 2",
+            locationId: 2,
+            diet: "Food",
+            img: ""
+        },
+        {
+            id: 3,
+            species: "Fish 3",
+            length: 3,
+            name: "Fish 3",
+            locationId: 3,
+            diet: "Food",
+            img: ""
+        },
+        {
+            id: 4,
+            species: "Fish 4",
+            length: 4,
+            name: "Fish 4",
+            locationId: 4,
+            diet: "Food",
+            img: ""
+        },
+        {
+            id: 5,
+            species: "Fish 5",
+            length: 5,
+            name: "Fish 5",
+            locationId: 5,
+            diet: "Food",
+            img: ""
+        },
+        {
+            id: 6,
+            species: "Fish 6",
+            length: 6,
+            name: "Fish 6",
+            locationId: 6,
+            diet: "Food",
+            img: ""
+        }
     ],
     locations = [
         {
-            id: ,
-            place: "",
-            quote: ""
+            id: 1,
+            place: "Location 1",
+            quote: "Quote 1"
         },
+        {
+            id: 2,
+            place: "Location 2",
+            quote: "Quote 2"
+        },
+        {
+            id: 3,
+            place: "Location 3",
+            quote: "Quote 3"
+        },
+        {
+            id: 4,
+            place: "Location 4",
+            quote: "Quote 4"
+        },
+        {
+            id: 5,
+            place: "Location 5",
+            quote: "Quote 5"
+        },
+        {
+            id: 6,
+            place: "Location 6",
+            quote: "Quote 6"
+        }
     ],
 }
 
